845c41c6def37a825e030020dcecd87ea3b2ff02
Wilmer Junior Santiago Donado <jwsantiago@uninorte.edu.co>
2023-09-18 20:13:56 -0500

    Creado libros

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from person import Autor
+from book import LibroImpreso, LibroDigital
 
 def main() -> None:
     nombre= input("Digite el nombre del autor")
@@ -6,5 +7,10 @@ def main() -> None:
     autor= Autor(nombre, cedula)
 
     print(autor)
+
+    libro= LibroImpreso(titulo= "Cien AÑos de Soledad", isbn= "12233333", genero= "Novela", formato="Tapa Dura", valor=80000.0, paginas=193, num_ejemplares= 450000)
+    libro2= LibroDigital(titulo= "Harry Potter", isbn= "12233377", genero= "Novela", formato="Tapa Dura", valor=50000.0, has_hipervinculo=False)
+
+    print (libro)
 if __name__ == '__main__':
     main()
